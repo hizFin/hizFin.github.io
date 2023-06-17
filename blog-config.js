e2a366673c98997870a9871c1475397d112d23b1
hizFin <cdwcdw34@gmail.com>
2023-06-17 17:51:51 +0900

    firstCommit

--- blog-config.js
+++ blog-config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: "hoodie",
-  description: "Hello :) I'm Hudi who developed gatsby-starter-hoodie theme.",
-  author: "Hudi",
-  siteUrl: "https://devhudi.github.io/gatsby-starter-hoodie/",
+  title: "hizFin 저장소",
+  description: "",
+  author: "hinzFin",
+  siteUrl: "https://hinzFin.com",
   links: {
-    github: "https://github.com/devHudi",
+    github: "https://github.com/hizFin",
     linkedIn: "https://linkedin.com",
     facebook: "https://www.facebook.com",
     instagram: "https://www.instagram.com",
-    email: "mailto:devhudi@gmail.com",
+    email: "mailto:ofin888@gmail.com",
   },
   utterances: {
-    repo: "devHudi/gatsby-starter-hoodie",
+    repo: "hizFin/hizFin.github.io",
     type: "pathname",
   },
 }
---
+++ contents/posts/first-post/index.md
@@ -0,0 +1,27 @@
+---
+title: "🚛 이사 완료"
+description:
+date: 2023-06-17
+update: 2023-06-17
+tags:
+  - blog
+  - gatsby
+series: "gatsby 블로그 시작하기"
+---
+
+## 이사 완료
+
+블로그 이사가 끝났다.
+
+기존 jekyll 테마(ruby 기반)를 사용하고 있었는데 Apple Silicon M1에서 계속 말썽을 일으켜서  
+gatsby 테마(react 기반)로 이사를 했다.
+
+새로운 집은 이미 공사가 완료된 아파트로 정했다. (빈 공터에서 집을 짓기에는 능력부족)  
+👍🏼 [gatsby-starter-hoodie](https://github.com/devHudi/gatsby-starter-hoodie) 👍🏼  
+
+
+이번 이사과정을 간략하게 나마 정리해서 올릴 예정이다.  
+이제 글도 꾸준히 올리고 화이팅 해야겠다!
+
+
+
